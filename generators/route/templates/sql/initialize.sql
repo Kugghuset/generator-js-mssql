b701145e12b082ec17a5cae6c369b42a1ddb14bd
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2016-03-31 17:11:22 +0200

    Fix capitalization on create table

--- generators/route/templates/sql/initialize.sql
+++ generators/route/templates/sql/initialize.sql
@@ -4,7 +4,7 @@ Initializes the <%= name %> table
 
 IF (OBJECT_ID('<%= nameCapitalized %>', 'U') IS NULL)
 BEGIN
-  CREATE TABLE [dbo].[<%= name %>] (
+  CREATE TABLE [dbo].[<%= nameCapitalized %>] (
       [<%= name %>Id] BigInt IDENTITY(1, 1) PRIMARY KEY NOT NULL
     , [description] VarChar(255) NULL
     , [dateCreated] DateTime2 DEFAULT GETUTCDATE() NULL
