d35e78b73eb9d04985b0cafd76f3309aa26f589d
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2016-04-04 19:22:15 +0200

    Fix correct path to config and utils in services

--- generators/service/templates/service.js
+++ generators/service/templates/service.js
@@ -3,8 +3,8 @@
 import _ from 'lodash';
 import Promise from 'bluebird';
 
-import config from '../config';
-import utils from '../utils/utils';
+import config from '../../config';
+import utils from '../../utils/utils';
 
 export default {
 
